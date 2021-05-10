ba19d53b561c6270b2444e9c4750da395390c006
slava <vkovalev@techmategroup.com>
2021-05-11 02:33:20 +0300

    work with db, pgadmin,rest api service in db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
+import { UsersModule } from "./users/users.module";
+import { RolesModule } from "./roles/roles.module";
+import { AuthModule } from "./auth/auth.module";
+
+console.log('process.env.POSTGRESS_PORT - ',process.env.POSTGRESS_PORT)
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve( __dirname, 'static'),
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      // models: [User, Role, UserRoles, Post],
+      autoLoadModels: true
+    }),
+    UsersModule,
+    RolesModule,
+    AuthModule,
+  ],
 })
+
 export class AppModule {}
